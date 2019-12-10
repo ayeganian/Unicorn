bc2e0901c25053b95ff4074923fc68a7e4a6958c
Nikita <ultranikit@gmail.com>
2019-12-10 14:58:15 +0200

    main-content title page

--- js/index.js
+++ js/index.js
@@ -41,3 +41,50 @@ let teamSwiper = new Swiper ('.swiper-container', {
         prevEl: '.swiper-button-prev',
     },
 });
+
+//main content sliders
+
+
+const swiperBusiness = new Swiper('.swiper-container-business', {
+    autoplay: {
+        delay: 3000,
+    },
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next-bus',
+        prevEl: '.swiper-button-prev-bus',
+    },
+});
+
+const swiperEducation = new Swiper('.swiper-container-education', {
+    autoplay: {
+        delay: 4000,
+    },
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next-ed',
+        prevEl: '.swiper-button-prev-ed',
+    },
+});
+
+const swiperSport = new Swiper('.swiper-container-sport', {
+    autoplay: {
+        delay: 5000,
+    },
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next-sp',
+        prevEl: '.swiper-button-prev-sp',
+    },
+});
+
+const swiperMarketing = new Swiper('.swiper-container-marketing', {
+    autoplay: {
+        delay: 3000,
+    },
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next-mark',
+        prevEl: '.swiper-button-prev-mark',
+    },
+});
