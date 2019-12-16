b6a3d46f4dc9069ff38c7eadcc829fb243f0f7ef
irina-kavun <kavunirina@gmail.com>
2019-12-16 17:08:52 +0200

    real estate sliders fixed

--- js/index.js
+++ js/index.js
@@ -28,36 +28,36 @@ closeIcon.addEventListener('click', () => {
 
 
 // Custom Select
-$(".custom-select").each(function() {
-    let classes = $(this).attr("class");
-    let template =  `<div class="${classes}">`;
-    template += `<span class="custom-select-trigger">Ru</span>`;
-    template += `<div class="custom-options">`;
-    $(this).find("option").each(function() {
-        template += `<span class="custom-option  ${$(this).attr("class")}" data-value="${$(this).attr("value")}">${$(this).html()}</span>`;
-    });
-    template += `</div></div>`;
-
-    $(this).wrap('<div class="custom-select-wrapper"></div>');
-    $(this).hide();
-    $(this).after(template);
-});
-$(".custom-option:first-of-type").hover(function() {
-    $(this).parents(".custom-options").addClass("option-hover");
-}, function() {
-    $(this).parents(".custom-options").removeClass("option-hover");
-});
-$(".custom-select-trigger").on("click", function() {
-    $('html').one('click',function() {
-        $(".custom-select").removeClass("opened");
-    });
-    $(this).parents(".custom-select").toggleClass("opened");
-    event.stopPropagation();
-});
-$(".custom-option").on("click", function() {
-    $(this).parents(".custom-select-wrapper").find("select").val($(this).data("value"));
-    $(this).parents(".custom-options").find(".custom-option").removeClass("selection");
-    $(this).addClass("selection");
-    $(this).parents(".custom-select").removeClass("opened");
-    $(this).parents(".custom-select").find(".custom-select-trigger").text($(this).text());
-});
+// $(".custom-select").each(function() {
+//     let classes = $(this).attr("class");
+//     let template =  `<div class="${classes}">`;
+//     template += `<span class="custom-select-trigger">Ru</span>`;
+//     template += `<div class="custom-options">`;
+//     $(this).find("option").each(function() {
+//         template += `<span class="custom-option  ${$(this).attr("class")}" data-value="${$(this).attr("value")}">${$(this).html()}</span>`;
+//     });
+//     template += `</div></div>`;
+//
+//     $(this).wrap('<div class="custom-select-wrapper"></div>');
+//     $(this).hide();
+//     $(this).after(template);
+// });
+// $(".custom-option:first-of-type").hover(function() {
+//     $(this).parents(".custom-options").addClass("option-hover");
+// }, function() {
+//     $(this).parents(".custom-options").removeClass("option-hover");
+// });
+// $(".custom-select-trigger").on("click", function() {
+//     $('html').one('click',function() {
+//         $(".custom-select").removeClass("opened");
+//     });
+//     $(this).parents(".custom-select").toggleClass("opened");
+//     event.stopPropagation();
+// });
+// $(".custom-option").on("click", function() {
+//     $(this).parents(".custom-select-wrapper").find("select").val($(this).data("value"));
+//     $(this).parents(".custom-options").find(".custom-option").removeClass("selection");
+//     $(this).addClass("selection");
+//     $(this).parents(".custom-select").removeClass("opened");
+//     $(this).parents(".custom-select").find(".custom-select-trigger").text($(this).text());
+// });
